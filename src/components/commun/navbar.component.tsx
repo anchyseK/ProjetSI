0cb2845d127f164e9ee2c5bd6b5b33af822ce9ac
Anchyse <kilian.anchyse@gmail.com>
2020-02-03 01:17:55 +0100

    Navigation bar modification
    Working on the sidebar

--- src/components/commun/navbar.component.tsx
+++ src/components/commun/navbar.component.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import { createStyles, makeStyles, Theme, createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import { Link as RouterLink, LinkProps as RouterLinkProps } from 'react-router-dom';
+import Link from '@material-ui/core/Link';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Link from '@material-ui/core/Link';
+import { Omit } from '@material-ui/types';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -36,34 +43,86 @@ const useStyles = makeStyles((theme: Theme) =>
       [theme.breakpoints.up('md')]: {
         marginLeft: theme.spacing(5),
       },
-    }
+    },
+    list: {
+      width: 250,
+    },
+    fullList: {
+      width: 'auto',
+    },
   }),
 );
 
 export default function Navbar() {
   const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  interface ListItemLinkProps {
+    primary: string;
+    to: string;
+  }
+
+  function ListItemLink(props: ListItemLinkProps) {
+    const { primary, to } = props;
+  
+    const renderLink = React.useMemo(
+      () =>
+        React.forwardRef<any, Omit<RouterLinkProps, 'to'>>((itemProps, ref) => (
+          <RouterLink to={to} ref={ref} {...itemProps} />
+        )),
+      [to],
+    );
+
+    return (
+      <li>
+        <ListItem button component={renderLink}>
+          <ListItemText primary={primary} />
+        </ListItem>
+      </li>
+    );
+  }
+
+  function handleDrawerToggle() {
+    setOpen(!open);
+  }
+
+  const drawer = (
+    <div>
+      <List
+        className={classes.list}
+        onClick={handleDrawerToggle}
+      >
+        <ListItemLink to="/" primary="Accueil" />
+        <ListItemLink to="/fomations" primary="Formations" />
+        <ListItemLink to="/rejoindre" primary="Nous rejoindre" />
+      </List>
+    </div>
+  );
 
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={handleDrawerToggle}>
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
             Projet SI
           </Typography>
           <nav>
-            <Link variant="button" color="inherit" href="#" className={classes.link}>
+            <Drawer open={open} onClose={handleDrawerToggle}>
+              {drawer}
+            </Drawer>
+            <Link variant="button" color="inherit" component={RouterLink} to="/" className={classes.link}>
               Accueil
             </Link>
-            <Link variant="button" color="inherit" href="#" className={classes.link}>
+            <Link variant="button" color="inherit" component={RouterLink} to="/formations" className={classes.link}>
               Formations
             </Link>
-            <Link variant="button" color="inherit" href="#" className={classes.link}>
+            <Link variant="button" color="inherit" component={RouterLink} to="/rejoindre" className={classes.link}>
               Nous rejoindre
             </Link>
-            <Button variant="outlined" color="inherit" href="#" className={classes.linkButton}>Se connecter</Button>
+            <Button variant="outlined" color="inherit" component={RouterLink} to="/connexion" className={classes.linkButton}>Se connecter</Button>
           </nav>
         </Toolbar>
       </AppBar>
