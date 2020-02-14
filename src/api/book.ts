959c614317c343c6aa4e09e2610db09e04067a0e
lpenaud <l.penaud@zaclys.net>
2020-02-14 09:56:07 +0100

    J'ai fait des trucs

--- src/api/book.ts
+++ src/api/book.ts
@@ -1,8 +1,20 @@
-import Book from '../../backend/src/models/book';
-import Connection from './index'
-import connection from './index';
+import { IBook } from '../../backend/src/models/book';
+import Connection from './index';
 
-export async function GetBooks(): Promise<Array<Book>> {
-    const res = await connection.get("/book")
-    return res.data;
+export async function GetBooks(): Promise<Array<IBook>> {
+    try {
+        const response = await Connection.get<IBook[]>("/book");
+        return response.data;
+    } catch (error) {
+        if (error.response) {
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+        } else if (error.request) {
+            console.log(error.request);
+        } else {
+            console.log('Error', error.message);
+        }
+        throw error;
+    }
 }
--- src/components/commun/books.component.tsx
+++ src/components/commun/books.component.tsx
@@ -1,3 +1,6 @@
-componentDidMount() {
-    getBooks().the
+/* https://fr.reactjs.org/docs/hooks-state.html */
+import React from "react";
+
+export default function Books() {
+    const [books, setBooks] = React.useState([]);
 }
